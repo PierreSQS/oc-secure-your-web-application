0491f8d1057cf9aaf706a2797fb787655b2aac1c
PierreSQS <nomo_messina@web.de>
2022-01-29 00:51:40 +0100

    added an assertion in test

--- src/test/java/com/openclassrooms/springsecurity/SpringSecurityAuthApplicationTests.java
+++ src/test/java/com/openclassrooms/springsecurity/SpringSecurityAuthApplicationTests.java
@@ -2,12 +2,16 @@ package com.openclassrooms.springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringSecurityAuthApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
