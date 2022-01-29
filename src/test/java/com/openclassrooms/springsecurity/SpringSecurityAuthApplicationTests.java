2dc9dc68072950e7825a192267847a9606cd43e8
PierreSQS <nomo_messina@web.de>
2022-01-30 00:09:46 +0100

    Commit02: refactored integration test

--- src/test/java/com/openclassrooms/springsecurity/SpringSecurityAuthApplicationTests.java
+++ src/test/java/com/openclassrooms/springsecurity/SpringSecurityAuthApplicationTests.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.springsecurity;
 
+import com.openclassrooms.springsecurity.controllers.LoginController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
@@ -9,9 +11,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class SpringSecurityAuthApplicationTests {
 
+	@Autowired
+	LoginController loginController;
+
 	@Test
 	void contextLoads(ApplicationContext appCtx) {
 		assertThat(appCtx).isNotNull();
+	}
+
+	@Test
+	void controllerIsInContext() {
+		assertThat(loginController).isNotNull();
 	}
 
 }
