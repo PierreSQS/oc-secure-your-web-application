c5b375a8c365ed3fa5dfdafa8307d9ccb1e935ca
PierreSQS <nomo_messina@web.de>
2022-01-30 07:50:30 +0100

    Commit03: renamed Config Class

--- src/main/java/com/openclassrooms/springsecurity/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/springsecurity/configuration/SpringSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
