10bd59a49a990744903816d4a01ceed3b9f44dd8
PierreSQS <nomo_messina@web.de>
2022-04-21 22:41:15 +0200

    Commit11: added a test with @WithUserDetails

--- src/test/java/com/openclassrooms/springsecurity/controllers/LoginControllerIT.java
+++ src/test/java/com/openclassrooms/springsecurity/controllers/LoginControllerIT.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.isA;
@@ -42,6 +44,15 @@ class LoginControllerIT {
         mockMvc.perform(formLogin("/login").user("springuser").password("springuser"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"))
+                .andDo(print());
+    }
+
+    @WithMockUser
+    @Test
+    void userWithMockUser() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome User!"))
                 .andDo(print());
     }
 
